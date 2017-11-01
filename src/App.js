af080dbefb454fb47e2c1cfed736f0a7cb7962f9
Marouane <faris.marouane@gmail.com>
2017-11-01 10:05:23 +0100

    Manage routes with react-router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
@@ -16,7 +17,8 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
+
+        <Route path='/search' render={ () => {return (
           <div className="search-books">
             <div className="search-books-bar">
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
@@ -37,7 +39,10 @@ class BooksApp extends React.Component {
               <ol className="books-grid"></ol>
             </div>
           </div>
-        ) : (
+
+          )}} />
+
+        <Route exact path='/' render={() => {return(
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -197,7 +202,11 @@ class BooksApp extends React.Component {
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
             </div>
           </div>
-        )}
+
+          ) }} />
+
+
+
       </div>
     )
   }
